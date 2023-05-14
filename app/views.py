fa7c1995d569759808eef20048969e41557023a6
mio256 <reiya@woorth.io>
2023-05-14 13:12:44 +0900

    :sparkles: views.IndexView / create base for TemplateResponse / TemplateView

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.http import JsonResponse
+from django.views.generic import TemplateView
 
 
 def PingView(request):
     return JsonResponse({'result': True})
+
+
+class IndexView(TemplateView):
+    template_name = "app/index.html"
--- local_gpt/urls.py
+++ local_gpt/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
+    path('', views.IndexView.as_view()),
     path('ping/', views.PingView),
     path('admin/', admin.site.urls),
 ]
