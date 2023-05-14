b874b546d5e9ceff552b334325ed4279948ec731
mio256 <reiya@woorth.io>
2023-05-14 12:26:56 +0900

    :wrench: get_random_secret_key in settings.py / protect secret key / import django.core.management.utils

--- local_gpt/settings.py
+++ local_gpt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8p^x#ov*!e9lw)x6us)^o_)&lqp$)^g08*#)nd)$as2a2s0-x='
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
