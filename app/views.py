74c2173cd677a5d3a866d875e45c690aaa86b9a0
mio256 <reiya@woorth.io>
2023-05-14 16:43:42 +0900

    :sparkles: post `next_message` / ask ChatGPT

--- app/views.py
+++ app/views.py
@@ -10,10 +10,14 @@ def PingView(request):
 class IndexView(TemplateView):
     template_name = "app/index.html"
 
+    def post(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        message = 'hello'
+        message = self.request.POST.get('next_message', 'hello')
+        context['your_message'] = message
         gpt_response = chatgpt_func.send_user_content(message)
         context['gpt_response'] = gpt_response['choices'][0]['message']['content']
 
