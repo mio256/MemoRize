4672aeedf3a1750fe05533e8efb3f36a41987ff8
mio256 <reiya@woorth.io>
2023-05-14 16:55:41 +0900

    :recycle: mv local_gpt/tests.py app/tests.py / :bug: wrong path

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,27 @@
+from app import chatgpt_func
 from django.test import TestCase
+from django.http import JsonResponse
+from django.template.response import TemplateResponse
 
-# Create your tests here.
+
+class PingTestCase(TestCase):
+    def testResponseType(self):
+        response = self.client.get('/ping/')
+        self.assertEqual(type(response), JsonResponse)
+
+    def testResponseContent(self):
+        response = self.client.get('/ping/')
+        self.assertEqual(response.json(), {'result': True})
+
+
+class IndexTestCase(TestCase):
+    def testResponseType(self):
+        response = self.client.get('/')
+        self.assertEqual(type(response), TemplateResponse)
+
+
+class ChatGPTTestCase(TestCase):
+    def testSendUserContentResponseType(self):
+        response = chatgpt_func.send_user_content('hello')
+        content = response['choices'][0]['message']['content']
+        self.assertEqual(type(content), str)
