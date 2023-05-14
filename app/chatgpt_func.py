5adf04f6dade76605d1a9cb054ecf477bf32ba91
mio256 <reiya@woorth.io>
2023-05-14 16:03:13 +0900

    :sparkles: chatgpt_func / use chatgpt  / import openai

---
+++ app/chatgpt_func.py
@@ -0,0 +1,21 @@
+import os
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+
+def send_messages(messages: list):
+    return openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+    )
+
+
+def send_user_content(content: str):
+    return send_messages([
+        {
+            "role": "user",
+            "content": content,
+        },
+    ])
